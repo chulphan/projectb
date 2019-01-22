d2457cad0827c26a001f49742daeb038cd7cd270
Neil Skaria <neil.skaria@gmail.com>
2019-01-21 21:56:07 -0600

    Commit 2

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,16 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "reactstrap";
+import Navbarr from "../components/Navbar";
 const Index = () => (
   <div>
-    <p>Hello World, the update worked.!</p>
+    <Navbarr />
+    <p>Hello Next.js</p>
+    <Button color="danger">Danger!</Button>
+    <p className="lead">
+      This is a simple hero unit, a simple Jumbotron-style component for calling
+      extra attention to featured content or information.
+    </p>
   </div>
 );
+
 export default Index;
